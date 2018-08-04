2b65804f9767e2b800f6c15d962b2c98caf5ce3b
Ahmad Aidil <aidil@kofera.com>
2018-08-05 01:38:45 +0700

    bug fixed .

--- src/App.js
+++ src/App.js
@@ -31,15 +31,17 @@ class App extends Component {
   }
 
   handleChange({ target: { name, value } }) {
-    runValidation(this.schema, { ...this.state.field, [name]: value })
+    this.setState(prevState => ({
+      field: { ...prevState.field, [name]: value }
+    }), () =>
+      runValidation(this.schema, { ...this.state.field, [name]: value })
       .then(schemaErrors => {
         this.setState(prevState => ({
-          field: { ...prevState.field, [name]: value },
           error: schemaErrors,
           touched: { ...prevState.touched, [name]: true },
           isValid: checkValidation(schemaErrors)
         }));
-      });
+      }));
   }
 
   handleSubmit(event) {
